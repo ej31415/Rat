725ececea8b54bf8f35176ecc63d4bcb10f404ae
Ovikx <ovikdas10@gmail.com>
2025-01-16 22:55:18 -0600

    convert mouse area node into characterbody2d node

--- gray_mouse.gd
+++ gray_mouse.gd
@@ -1,41 +1,47 @@
-extends Player
+extends CharacterBody2D
 
-signal hit
+enum Roles {MOUSE, RAT, SHERIFF}
 
-var screen_size # size of screen window
+const SPEED = 400.0
+var anim = "front"
+var role = Roles.RAT
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	screen_size = get_viewport_rect().size
-	hide()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	# Set movement speed
-	var movement = move($AnimatedSprite2D.animation)
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_vector("LEFT", "RIGHT", "UP", "DOWN").normalized()
+	if role == Roles.RAT and Input.is_action_pressed("SHIFT"):
+		direction *= 2.0
+		$AnimatedSprite2D.speed_scale = 2.0
+	else:
+		$AnimatedSprite2D.speed_scale = 1.0
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity = Vector2.ZERO
 	
-	var velocity = movement[0]
-		
+	move_and_slide()
+	
+	# Set sprite orientation
+	if velocity.x < 0:
+		anim = "left"
+	elif velocity.y > 0:
+		anim = "front"
+	elif velocity.y < 0:
+		anim = "gyatt"
+	elif velocity.x > 0:
+		anim = "right"
+	else:
+		if anim == "left":
+			anim = "static left"
+		elif anim == "right":
+			anim = "static right"
+		elif anim == "gyatt":
+			anim = "static gyatt"
+		elif anim == "front":
+			anim = "static front"
+	
+	$AnimatedSprite2D.animation = anim
 	if velocity.length() > 0:
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
-		
-	# Set position and limit to screen
-	position += (velocity * delta)
-	
-	$AnimatedSprite2D.animation = movement[1]
-	$AnimatedSprite2D.speed_scale = movement[2]
-
-
-func _on_body_entered(body: Node2D) -> void:
-	hit.emit()
-	$CollisionShape2D.set_deferred("disabled", true) # make sure signal only sends once
-	
-func start(pos):
-	# pass in the start position from spawner as pos?
-	position = pos
-	show()
-	$CollisionShape2D.disabled = true
--- main.gd
+++ main.gd
@@ -2,9 +2,9 @@ extends Node
 
 # Constructed for testing purposes only.
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$GrayMouse.start($StartPosition.position)
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#$GrayMouse.start($StartPosition.position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
