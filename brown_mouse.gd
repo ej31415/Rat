3c1a2cc1bc2c945ad56e912c0ed408a5eccc8145
Shivam Pathak <58636209+pathaks1@users.noreply.github.com>
2025-01-19 22:18:11 -0600

    simplified camera focus

--- brown_mouse.gd
+++ brown_mouse.gd
@@ -2,8 +2,5 @@ extends Player
 
 func _ready() -> void:
 	print("initialized brown mouse")
-
-func _physics_process(delta: float) -> void:
 	if is_multiplayer_authority():
 		$Camera2D.make_current()
-	super._physics_process(delta)
