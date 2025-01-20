336b24fd4a2ef51f209268bc66ad1d4441e9690b
Shivam Pathak <58636209+pathaks1@users.noreply.github.com>
2025-01-19 22:14:30 -0600

    synced other colors

--- sb_mouse.gd
+++ sb_mouse.gd
@@ -2,3 +2,8 @@ extends Player
 
 func _ready() -> void:
 	print("initialized strawberry blonde mouse")
+
+func _physics_process(delta: float) -> void:
+	if is_multiplayer_authority():
+		$Camera2D.make_current()
+	super._physics_process(delta)
