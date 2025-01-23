f37d3e84cbb7937d5c16ecde7420085fb60e2d59
Eric J <ericj31415@gmail.com>
2025-01-23 00:07:21 -0600

    reset map

--- main.gd
+++ main.gd
@@ -9,6 +9,7 @@ var mice = []
 var color_to_role = {}
 var color_to_instance = {}
 var game_ended = false
+var is_host = false
 
 func _init() -> void:
 	gray_mouse = preload("res://gray_mouse.tscn")
@@ -23,6 +24,7 @@ func _on_host_pressed():
 	multiplayer.peer_connected.connect(_add_player)
 	_add_player()
 	$StartMenu/start.disabled = false
+	is_host = true
 
 func _add_player(id = 1):
 	if len(mice) > 0:
@@ -48,11 +50,8 @@ func _on_start_pressed():
 
 @rpc("call_local")
 func start_helper(maze: Array, offset: Vector2i, true_roles: Dictionary):
-	$StartMenu/label.visible = false
-	$StartMenu/ip.visible = false
-	$StartMenu/host.visible = false
-	$StartMenu/join.visible = false
-	$StartMenu/start.visible = false
+	set_process(false)
+	$StartMenu.visible = false
 	$Map.erase_maze(maze, offset)
 	$Map.build_maze(maze, offset)
 	
@@ -65,17 +64,37 @@ func start_helper(maze: Array, offset: Vector2i, true_roles: Dictionary):
 			var temp = child.starter(true_roles)
 			if temp != "":
 				role = temp
+				
+	for player in get_tree().get_nodes_in_group("player"):
+		player.global_position = Vector2(0, 0)
+		
 	$HUD/Role.text = "You are a " + role + ". . ."
 	$TimerCanvasLayer.start(1000*120)
+	$WinScreen/MiceWin.visible = false
+	$WinScreen/Again.visible = false
+	game_ended = false
+	set_process(true)
 
 func _end_game() -> void:
+	print("game ended!!!")
 	game_ended = true
 	$TimerCanvasLayer.end_timer.rpc()
 	$WinScreen/MiceWin.visible = true
+	if is_host:
+		$WinScreen/Again.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	for player in get_tree().get_nodes_in_group("player"):
 		var player_tile = $Map/Exit.local_to_map(player.global_position)
 		if $Map/Exit.get_cell_source_id(player_tile) != -1 and not game_ended and player.get_role() != "rat":
+			print(str($Map/Exit.get_cell_source_id(player_tile) != -1) + " " + str(game_ended) + " in " + str(is_host))
+			print($Map/Exit.local_to_map(player.global_position))
 			_end_game()
+
+
+func _on_again_button_pressed() -> void:
+	$Map._ready()
+	var maze = $Map.get_maze()
+	var offset = $Map.get_offset()
+	start_helper.rpc(maze, offset, color_to_role)
