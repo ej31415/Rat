8adf0d3fcb108e0702c0252949f9d90dedf3df28
Eric J <ericj31415@gmail.com>
2025-01-26 17:49:19 -0600

    just a bunch of stuff lowkey

--- brown_mouse.gd
+++ brown_mouse.gd
@@ -9,6 +9,10 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "attack":
 		print(color + " finished attack animation")
 		set_physics_process(true)
+		$AnimatedSprite2D.modulate = Color(1, 152/255.0, 147/255.0, 1)
 	if "shoot" in anim_name:
 		print(color + " finished shooting animation")
 		set_physics_process(true)
+		$AnimatedSprite2D.modulate = Color(1, 152/255.0, 147/255.0, 1)
+	if anim_name == "die":
+		$AnimatedSprite2D.animation = "die"
