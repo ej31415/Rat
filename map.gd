eb1cfca1c98ffd3eb91b29918f20bf1e8a76b48c
Ovikx <ovikdas10@gmail.com>
2025-01-26 15:00:55 -0600

    increase maze size

--- map.gd
+++ map.gd
@@ -223,7 +223,7 @@ func erase_maze(mat: Array, offset: Vector2i):
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var maze := _horizontal_stretch(_vertical_stretch(generate_maze(5, 5), 2), 2)
+	var maze := _horizontal_stretch(_vertical_stretch(generate_maze(8, 8), 2), 2)
 	maze = _add_end_tiles(_adjust_wall_types(_extend_outer_walls(maze, 4)))
 	_pretty_print_mat(maze)
 	var offset := Vector2i(-len(maze[0])/2, -len(maze)-1)
