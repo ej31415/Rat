ed4a9b61ae07f7c41a2c5e92ab85ddba9da4d730
Shivam Pathak <58636209+pathaks1@users.noreply.github.com>
2025-02-07 15:33:32 -0600

    disconnect button and fixed server discon

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 class_name Player
 
 static var roles = ["mouse", "mouse", "rat", "sheriff"]
+static var roles_copy = ["mouse", "mouse", "rat", "sheriff"]
 static var rng = RandomNumberGenerator.new()
 
 const SPEED = 600.0
