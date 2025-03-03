46cb5969ddde17bf661374361275231e4f8b85ca
Ovikx <ovikdas10@gmail.com>
2025-03-03 15:34:08 -0600

    fix show leaderboard being called multiple times

--- main.gd
+++ main.gd
@@ -482,6 +482,7 @@ func _end_game(mice_win: bool, sheriff_win: bool, time_out: bool, player_discon:
 			$WinScreen/Again.disabled = true
 			await get_tree().create_timer(2).timeout
 			show_leaderboard()
+			break
 	
 	if is_host: # allow only host to start new game
 		$WinScreen/Again.visible = true
